2bb03ecc991d6a732184e2580ac23815fdf89ff1
TConnellan <thomasjconnellan@gmail.com>
2024-07-24 21:37:53 +1000

    hash index for email column, TODO load test comparison with B-Tree

--- migrations/000002_create_users_indexes.up.sql
+++ migrations/000002_create_users_indexes.up.sql
@@ -1,3 +1,1 @@
-CREATE INDEX IF NOT EXISTS idx_users_username ON users (name);
-CREATE INDEX IF NOT EXISTS idx_users_email ON users (email);
--- these could be made on a hash column of these values
+CREATE INDEX IF NOT EXISTS idx_users_email ON users USING HASH(email);
