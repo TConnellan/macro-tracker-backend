60b66c9f73f5a432a6c5de04a098ffd7f2cc515d
TConnellan <thomasjconnellan@gmail.com>
2024-11-01 23:56:00 +1100

    email/pasword helper func tests

--- internal/data/users_test.go
+++ internal/data/users_test.go
@@ -5,7 +5,203 @@ import (
 	"testing"
 
 	"github.com/tconnellan/macro-tracker-backend/internal/assert"
+	"github.com/tconnellan/macro-tracker-backend/internal/validator"
 )
+
+func TestPasswordHelpers(t *testing.T) {
+
+	tests := []struct {
+		name           string
+		pass           string
+		valid          bool
+		expectPassword password
+		checkAgainst   string
+		matches        bool
+		matchesError   error
+	}{
+		{
+			name:         "valid password",
+			pass:         "AbCdEfGh123!@",
+			valid:        true,
+			checkAgainst: "AbCdEfGh123!@",
+			matches:      true,
+			matchesError: nil,
+		},
+		{
+			name:         "valid password mismatch compare not equal",
+			pass:         "AbCdEfGh123!@",
+			valid:        true,
+			checkAgainst: "something else",
+			matches:      false,
+			matchesError: nil,
+		},
+		{
+			name:         "valid password mismatch compare upper case",
+			pass:         "AbCdEfGh123!@",
+			valid:        true,
+			checkAgainst: "ABCDEFGH123!@",
+			matches:      false,
+			matchesError: nil,
+		},
+		{
+			name:         "valid password mismatch compare lower case",
+			pass:         "AbCdEfGh123!@",
+			valid:        true,
+			checkAgainst: "abcdefgh123!@",
+			matches:      false,
+			matchesError: nil,
+		},
+		{
+			name:  "invalid password empty",
+			pass:  "",
+			valid: false,
+		},
+		{
+			name:  "invalid password short",
+			pass:  "AbCd",
+			valid: false,
+		},
+		{
+			name:  "invalid password long",
+			pass:  "AbCdEfGh123!@AbCdEfGh123!@AbCdEfGh123!@AbCdEfGh123!@AbCdEfGh123!@AbCdEfGh123!@",
+			valid: false,
+		},
+	}
+
+	// of interest, also possible for bcrypt to return other errors on comparison, if the hash
+	// is too short/long/malformatted or the cost is not in a valid range (4 - 31)
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			v := validator.New()
+
+			ValidatePasswordPlaintext(v, tt.pass)
+			if v.Valid() != tt.valid {
+				assert.Equal(t, v.Valid(), tt.valid)
+			}
+			if !v.Valid() {
+				return
+			}
+
+			actualPassword := password{}
+			err := actualPassword.Set(tt.pass)
+			assert.ExpectError(t, err, nil)
+			if err != nil {
+				return
+			}
+
+			assert.Equal(t, *actualPassword.plaintext, tt.pass)
+
+			matches, err := actualPassword.Matches(tt.checkAgainst)
+			assert.ExpectError(t, err, tt.matchesError)
+			assert.Equal(t, matches, tt.matches)
+
+		})
+	}
+}
+
+func TestEmailHelpers(t *testing.T) {
+
+	tests := []struct {
+		name  string
+		email string
+		valid bool
+	}{
+		{
+			name:  "valid email",
+			email: "john.doe@gmail.com",
+			valid: true,
+		},
+		{
+			name:  "valid email 2",
+			email: "john.doe-second@gmail.com",
+			valid: true,
+		},
+		{
+			name:  "valid email 3",
+			email: "john_doe@gmail.com",
+			valid: true,
+		},
+		{
+			name:  "valid email 4",
+			email: "john.doe@gmail-test.com",
+			valid: true,
+		},
+		{
+			name:  "invalid email",
+			email: "john.doe",
+			valid: false,
+		},
+		{
+			name:  "invalid empty email",
+			email: "",
+			valid: false,
+		},
+		{
+			name:  "invalid consecutive periods",
+			email: "john.doe@gmail.com",
+			valid: false,
+		},
+		{
+			name:  "invalid no characters",
+			email: ".@gmail.com",
+			valid: false,
+		},
+		{
+			name:  "invalid trailing special character 1",
+			email: "john.doe-@gmail.com",
+			valid: false,
+		},
+		{
+			name:  "invalid trailing special character 2",
+			email: "john.doe_@gmail.com",
+			valid: false,
+		},
+		{
+			name:  "invalid trailing special character 3",
+			email: "john.doe.@gmail.com",
+			valid: false,
+		},
+		{
+			name:  "invalid no top level domain",
+			email: "john.doe@gmail",
+			valid: false,
+		},
+		{
+			name:  "invalid top level domain qualifier two short",
+			email: "john.doe@gmail.c",
+			valid: false,
+		},
+		{
+			name:  "invalid no email domain",
+			email: "john.doe@.com",
+			valid: false,
+		},
+		{
+			name:  "invalid email domain",
+			email: "john.doe@-gmail.com",
+			valid: false,
+		},
+		{
+			name:  "invalid email domain 2",
+			email: "john.doe@gmail.com-",
+			valid: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			v := validator.New()
+
+			ValidateEmail(v, tt.email)
+			if v.Valid() != tt.valid {
+				assert.ValidatorValid(t, v)
+			}
+
+		})
+	}
+}
 
 func TestUserModelExists(t *testing.T) {
 
